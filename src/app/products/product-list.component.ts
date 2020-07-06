c365a004d5ce75fdbb6f217f85cc090801383859
Anirudh <anirudhmadhavan1994@gmail.com>
2020-07-06 19:31:25 +0800

    Added filtering, simple extra style, prettier package support, new custom pipe, type support for product

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,73 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
+import { FileDetector } from 'protractor';
 
 @Component({
-    selector : 'pm-products',
-    //Linked template
-    templateUrl :'./product-list.component.html'
+   selector: 'pm-products',
+   //Linked template
+   templateUrl: './product-list.component.html',
+   //Linked CSS
+   styleUrls: ['./product-list.component.css'],
 })
+export class ProductListComponent implements OnInit {
+   ngOnInit(): void {
+      console.log('OnInit');
+   }
 
-export class ProductListComponent {
-    pageTitle : string = 'Product List';
-    imageWidth : number = 50;
-    imageMargin : number = 2;
-    showImage : boolean = false;
-    filteredBy : string = '';
-    
-    products : Product[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2019",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2019",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2019",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        },
-        {
-          "productId": 8,
-          "productName": "Saw",
-          "productCode": "TBX-0022",
-          "releaseDate": "May 15, 2019",
-          "description": "15-inch steel blade hand saw",
-          "price": 11.55,
-          "starRating": 3.7,
-          "imageUrl": "assets/images/saw.png"
-        },
-        {
-          "productId": 10,
-          "productName": "Video Game Controller",
-          "productCode": "GMG-0042",
-          "releaseDate": "October 15, 2018",
-          "description": "Standard two-button video game controller",
-          "price": 35.95,
-          "starRating": 4.6,
-          "imageUrl": "assets/images/xbox-controller.png"
-        }
-      ];
+   constructor() {
+      this.filteredProducts = this.products;
+      this.filteredBy = '';
+   }
 
-      toggleImage() : void {
-          this.showImage = !this.showImage;
-      }
+   pageTitle: string = 'Product List';
+   imageWidth: number = 50;
+   imageMargin: number = 2;
+   showImage: boolean = false;
+
+   _filteredBy: string = '';
+
+   products: Product[] = [
+      {
+         productId: 1,
+         productName: 'Leaf Rake',
+         productCode: 'GDN-0011',
+         releaseDate: 'March 19, 2019',
+         description: 'Leaf rake with 48-inch wooden handle.',
+         price: 19.95,
+         starRating: 3.2,
+         imageUrl: 'assets/images/leaf_rake.png',
+      },
+      {
+         productId: 2,
+         productName: 'Garden Cart',
+         productCode: 'GDN-0023',
+         releaseDate: 'March 18, 2019',
+         description: '15 gallon capacity rolling garden cart',
+         price: 32.99,
+         starRating: 4.2,
+         imageUrl: 'assets/images/garden_cart.png',
+      },
+      {
+         productId: 5,
+         productName: 'Hammer',
+         productCode: 'TBX-0048',
+         releaseDate: 'May 21, 2019',
+         description: 'Curved claw steel hammer',
+         price: 8.9,
+         starRating: 4.8,
+         imageUrl: 'assets/images/hammer.png',
+      },
+      {
+         productId: 8,
+         productName: 'Saw',
+         productCode: 'TBX-0022',
+         releaseDate: 'May 15, 2019',
+         description: '15-inch steel blade hand saw',
+         price: 11.55,
+         starRating: 3.7,
+         imageUrl: 'assets/images/saw.png',
+      },
+      {
+         productId: 10,
+         productName: 'Video Game Controller',
+         productCode: 'GMG-0042',
+         releaseDate: 'October 15, 2018',
+         description: 'Standard two-button video game controller',
+         price: 35.95,
+         starRating: 4.6,
+         imageUrl: 'assets/images/xbox-controller.png',
+      },
+   ];
+   filteredProducts: Product[];
+
+   toggleImage(): void {
+      this.showImage = !this.showImage;
+   }
+
+   //This is called by the Filtered By text field in html to display
+   get filteredBy() {
+      return this._filteredBy;
+   }
+
+   //This is called when user changes input and two way binding takes effect
+   set filteredBy(filteredBy: string) {
+      this._filteredBy = this.filteredBy;
+      this.filteredProducts = this.products.filter(
+         (product) =>
+            product.productName.toLocaleLowerCase().indexOf(filteredBy.toLocaleLowerCase()) !== -1
+      );
+   }
 }
